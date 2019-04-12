4996dac659a37542162dd37731bb8cb0ccba3000
NikitaGlukhi <nikita.glukhi@valor-software.com>
2019-04-12 13:14:34 +0300

    (refactor): frontend unit tests reactored

--- src/common/database.providers.ts
+++ src/common/database.providers.ts
@@ -5,7 +5,7 @@ export const databaseProviders = [
     provide: 'DbConnectionToken',
     useFactory: async (): Promise<typeof mongoose> =>
       await mongoose.connect(
-        `${ process.env.MONGO_URL || process.env.MONGO_URL_LOCAL }/repositories_versions`,
+        `${ process.env.MONGO_URL }/repositories_versions`,
         { useNewUrlParser: true, useFindAndModify: false },
       ),
   }
--- src/static/main.ts
+++ src/static/main.ts
@@ -1,6 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
 import { FrontendModule } from './frontend.module';
+import { environment } from './environments/environment';
+
+if(environment.production) {
+    enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(FrontendModule)
   .catch(err => console.error(err));
