d55006485003b17e4f65bdecd807c805afbbeffc
bear <mikhail.nalimov@valor-software.com>
2019-07-05 15:09:16 +0300

    deploy to appengine

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 PROJECT_NAME=git-repo-manager
 DOCKER_IMAGE_NAME=git-repo
 ZONE_EN1B=us-east1-b
+PORT=8080
 
 echo "docker build"
 docker build -t gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT \
@@ -22,9 +23,11 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 gcloud --quiet config set project $PROJECT_NAME
 gcloud --quiet config set compute/zone $ZONE_EN1B
 
-echo "docker push"
-gcloud docker -- push gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT
-yes | gcloud beta container images add-tag gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:latest
+gcloud app deploy 
+
+# echo "docker push"
+# gcloud docker -- push gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT
+# yes | gcloud beta container images add-tag gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:latest
 
 # echo "Create instance"
 # gcloud beta compute instances create-with-container instance-1 \
@@ -33,10 +36,10 @@ yes | gcloud beta container images add-tag gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NA
 #   --machine-type=n1-standard-2 \
 #   --tags app
 
-echo "Update instance"
-gcloud beta compute instances update-container instance-1 \
-  --zone $ZONE_EN1B \
-  --container-image=gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:latest \
+# echo "Update instance"
+# gcloud beta compute instances update-container instance-1 \
+#   --zone $ZONE_EN1B \
+#   --container-image=gcr.io/$PROJECT_NAME/$DOCKER_IMAGE_NAME:latest \
 
 # echo "Create firewall-rules"
 # gcloud compute firewall-rules create port-forwarding-3000 \
