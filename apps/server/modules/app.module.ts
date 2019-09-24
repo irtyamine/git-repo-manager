d932aad73ff718a2fa0bcb6bd2af76937efa7cd6
NikitaGlukhi <nikita.glukhi@valor-software.com>
2019-09-24 09:56:05 +0300

    feat(core): implemented controller, that provide frontend loading from server

--- apps/server/modules/app.module.ts
+++ apps/server/modules/app.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { GithubRepositoriesModule } from './github-repositories/github-repositories.module';
+import { ClientSideModule } from './client-side/client-side.module';
+import { ClientSideMiddleware } from '../middlewares/client-side.middleware';
 
 @Module({
   imports: [
     GithubRepositoriesModule,
+    ClientSideModule
   ],
   exports: [],
   providers: []
 })
 
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(ClientSideMiddleware)
+      .forRoutes({
+        path: '/',
+        method: RequestMethod.ALL
+      });
+  }
+}
--- apps/server/modules/main.ts
+++ apps/server/modules/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
 import 'dotenv/config';
 import * as compression from 'compression';
 
@@ -9,7 +10,8 @@ const server = express();
 server.use(compression());
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, server, {});
+  app.setBaseViewsDir(join(__dirname, '..', 'client'));
   await app.listen(3000);
 }
 bootstrap();
