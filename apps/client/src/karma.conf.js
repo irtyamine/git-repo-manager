3476918d2f40c7a9335682d500dfd828682c850a
NikitaGlukhi <nikita.glukhi@valor-software.com>
2019-11-12 10:54:28 +0200

    feat(client): set Karma config

--- apps/client/src/karma.conf.js
+++ apps/client/src/karma.conf.js
@@ -15,18 +15,51 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/client'),
+      dir: require('path').join(__dirname, '../../../coverage/client'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+      fixWebpackSourcePaths: true,
+      combineBrowserReports: true,
+      skipFilesWithNoCoverage: false,
+      'report-config': {
+        html: {
+          subdir: 'html'
+        }
+      },
+      thresholds: {
+        emitWarning: false,
+        global: {
+          statements: 20,
+          lines: 20,
+          branches: 20,
+          functions: 20
+        },
+        each: {
+          statements: 20,
+          lines: 20,
+          branches: 20,
+          functions: 20
+        }
+      },
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome'],
+      singleRun: false,
+      restartOnFileChange: true,
+      customLaunchers: {
+        ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            ' --remote-debugging-port=9222'
+          ]
+        }
+      }
+    }
   });
 };
